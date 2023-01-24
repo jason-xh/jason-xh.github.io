5cbc8d1c60e24c0d1d8034d8dd22c9ab4e92e321
Jason Xiao <jaesonxh@gmail.com>
2023-01-24 12:54:48 +1100

    backup personal work

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 
-import { useEffect, useState, useReducer } from "react";
+import React, { useEffect, useState, useReducer } from "react";
+import { ProSidebarProvider } from 'react-pro-sidebar';
 
-import Navigation from './components/Navigation';
+import Sidebar from './components/Sidebar/Sidebar';
+import NavigationBar from './components/NavigationBar/NavigationBar';
 
 function App() {
   const [state, setState] = useState("1");
@@ -23,33 +25,45 @@ function App() {
   }, [state2]);
 
   return (
-    <div className="App">
-      <Navigation/>
-      <h1>Current state is {state}</h1>
-      <button onClick={() => setState("0")}>
-        Set state 0
-      </button>
-      <button onClick={() => setState("1")}>
-        Set state 1
-      </button>
-      <h2>Current state2 is {state2}</h2>
-      <button onClick={() => setState2("a")}>
-        Set state a
-      </button>
-      <button onClick={() => setState2("b")}>
-        Set state b
-      </button>
+    <ProSidebarProvider>
+      <div className='ViewPort'>
+        {/* <NavigationBar></NavigationBar> */}
+        <Sidebar></Sidebar>
+        <div className="App" id="outer-container">
+          <div id="page-wrap">
 
-      <h2>Checkbox</h2>
-      <input 
-        type="checkbox"
-        value={checked}
-        onChange={setChecked}
-      />
-      <label>
-        {checked ? "checked" : "not checked"}
-      </label>
-    </div>
+            <h1>test</h1>
+            
+            <h1>Current state is {state}</h1>
+            <button onClick={() => setState("0")}>
+              Set state 0
+            </button>
+            <button onClick={() => setState("1")}>
+              Set state 1
+            </button>
+            <h2>Current state2 is {state2}</h2>
+            <button onClick={() => setState2("a")}>
+              Set state a
+            </button>
+            <button onClick={() => setState2("b")}>
+              Set state b
+            </button>
+
+            <h2>Checkbox</h2>
+            <input 
+              type="checkbox"
+              value={checked}
+              onChange={setChecked}
+            />
+            <label>
+              {checked ? "checked" : "not checked"}
+            </label>
+            <p>
+            </p>
+          </div>
+        </div>
+      </div>
+    </ProSidebarProvider>
   );
 }
 
