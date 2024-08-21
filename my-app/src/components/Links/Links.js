7dad6c47ee01d230647cdde49a8316c46be7cef9
Jason Xiao (Hidajat) <jaesonxh@gmail.com>
2024-08-22 00:22:40 +1000

    updated site

--- my-app/src/components/Links/Links.js
+++ my-app/src/components/Links/Links.js
@@ -9,31 +9,46 @@ import buttonImg from "./Data/drawnButton.png";
 
 // export default props => {
 export default function Links() {
-    const [bandDisplay, changeBandDisplay] = useState('none');
+    const [psDisplay, changePsDisplay] = useState('none');
+    const [wgDisplay, changeWgDisplay] = useState('none');
     const [djDisplay, changeDjDisplay] = useState('none');
     return (
         <div className="links-wrapper">
             {/* <a href="https://www.tiktok.com/@kerakeiz/video/7144024534239153410" target="_blank" className="link-btn">
                 <p>pretty stray (big things coming)</p>
             </a> */}
-            <a onClick={() => (bandDisplay) == 'none' ? changeBandDisplay('flex') : changeBandDisplay('none')} className="band-expand-link">
-                <p>{(bandDisplay) == 'none' ? "▶" : "▼"} band: pretty stray (big things coming)</p>
+            <a onClick={() => (psDisplay) == 'none' ? changePsDisplay('flex') : changePsDisplay('none')} className="band-expand-link">
+                <p>{(psDisplay) == 'none' ? "▶" : "▼"} band: pretty stray</p>
             </a>
-            <div className="band-links-wrapper" style={{display: bandDisplay}}>
+            <div className="band-links-wrapper" style={{display: psDisplay}}>
                 <a href="https://www.tiktok.com/@kerakeiz/video/7144024534239153410" target="_blank" className='band-link'>metric - black sheep (cover)</a>
+                <a href="https://www.tiktok.com/@kerakeiz/video/7250323452450639105" target="_blank" className='band-link'>smashing pumpkins - mayonaise (cover) on digicam</a>
+                <a href="https://www.tiktok.com/@kerakeiz/video/7247545198081805569" target="_blank" className='band-link'>smashing pumpkins - mayonaise (cover)</a>
                 <a href="https://www.tiktok.com/@kerakeiz/video/7137924792656694529" target="_blank" className='band-link'>yuragi - night is young (cover)</a>
                 <a href="https://www.tiktok.com/@kerakeiz/video/7142140742834605313" target="_blank" className='band-link'>yuragi - sleeptight (cover)</a>
                 <a href="https://www.tiktok.com/@psu.ae/video/7171943272334593282" target="_blank" className='band-link'>kinokoteikoku - ヴァージン・スーサイド (cover)</a>
+                <a href="https://www.tiktok.com/@kerakeiz/video/7255727479007104257" target="_blank" className='band-link'>笹川真生 (mao sasagawa) - さめない (samenai) (cover)</a>
+            </div>
+            <a onClick={() => (wgDisplay) == 'none' ? changeWgDisplay('flex') : changeWgDisplay('none')} className="band-expand-link">
+                <p>{(wgDisplay) == 'none' ? "▶" : "▼"} band: wasabi galaxy (upcoming gigs below!)</p>
+            </a>
+            <div className="band-links-wrapper" style={{display: wgDisplay}}>
+                <a href="https://www.instagram.com/wasabigalaxyband/" target="_blank" className='band-link'>band instagram</a>
+                <a href="https://events.humanitix.com/spooktember" target="_blank" className='band-link'>animesydney spooktember 13/9 @ unsw roundhouse</a>
+                <a href="https://lazyboneslounge.com.au/" target="_blank" className='band-link'>??? 26/8 @ lazybones marrickville</a>
+                <a href="https://www.instagram.com/wasabigalaxyband/" target="_blank" className='band-link'>??? 30/11 @ ???</a>
             </div>
             <a onClick={() => (djDisplay) == 'none' ? changeDjDisplay('flex') : changeDjDisplay('none')} className="dj-expand-link">
                 <p>{(djDisplay) == 'none' ? "▶" : "▼"} dj mixes</p>
             </a>
             <div className="dj-links-wrapper" style={{display: djDisplay}}>
-                <a href="https://soundcloud.com/user-417328483/whiplash-mix-live-at-dazcon-2023" target="_blank" className='dj-link'>whiplash mix (live at dazcon 2023)</a>
-                <a href="https://soundcloud.com/user-417328483/fever-dream" target="_blank" className='dj-link'>fever dream mix</a>
+                <a href="https://soundcloud.com/xhmusic2/djxh-your-shot-2024-wildcard-set-live-on-red-bull-stage-blue" target="_blank" className='dj-link'>your shot 2024 full wildcard set (live on red bull stage Red)</a>
+                <a href="https://soundcloud.com/xhmusic2/dvj-debut-live-at-x_360_noscope_420_x" target="_blank" className='dj-link'>dvj debut meme mix (live at x_360_NOSCOPE_420_X)</a>
+                <a href="https://soundcloud.com/xhmusic2/whiplash-mix-live-at-dazcon-2023" target="_blank" className='dj-link'>whiplash mix (live at dazcon 2023)</a>
+                <a href="https://soundcloud.com/xhmusic2/fever-dream" target="_blank" className='dj-link'>fever dream (first mix)</a>
             </div>
-            <a href="https://www.instagram.com/87.prelude/" target="_blank" className="link-btn">
-                <p>my shitty car</p>
+            <a href="https://www.instagram.com/ihatecars1_/" target="_blank" className="link-btn">
+                <p>shitty car posting</p>
             </a>
             {/* <a href="" target="_blank" className="link-btn">
                 <img src={buttonImg} className="link-img"/>
